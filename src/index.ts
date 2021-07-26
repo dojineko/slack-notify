91d58a82649453656bd82cdf9844b198689c43b3
dojineko <dojineko.spiral@gmail.com>
2021-07-26 12:13:25 +0900

    bonsai

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ void (async () => {
   const jobStatus = core.getInput('job_status')
   const slackWebhook = core.getInput('slack_webhook')
   const channel = core.getInput('channel', { required: false })
-  const icon_emoji = core.getInput('icon_emoji', { required: false })
+  const iconEmoji = core.getInput('icon_emoji', { required: false })
 
   const repo = `${github.context.repo.owner}/${github.context.repo.repo}`
   const actor = github.context.actor
@@ -60,7 +60,7 @@ void (async () => {
   const slack = new IncomingWebhook(slackWebhook)
   await slack.send({
     channel: channel,
-    icon_emoji: icon_emoji,
+    icon_emoji: iconEmoji,
     attachments: [
       {
         text: [
