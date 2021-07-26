2f2844ecae0b4a8695ef5a120e2c96c7d5ca6364
dojineko <dojineko.spiral@gmail.com>
2021-07-26 12:13:30 +0900

    build

--- dist/index.js
+++ dist/index.js
@@ -86,7 +86,7 @@ void (() => __awaiter(void 0, void 0, void 0, function* () {
     const jobStatus = core.getInput('job_status');
     const slackWebhook = core.getInput('slack_webhook');
     const channel = core.getInput('channel', { required: false });
-    const icon_emoji = core.getInput('icon_emoji', { required: false });
+    const iconEmoji = core.getInput('icon_emoji', { required: false });
     const repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
     const actor = github.context.actor;
     const workflow = github.context.workflow;
@@ -99,7 +99,7 @@ void (() => __awaiter(void 0, void 0, void 0, function* () {
     const slack = new webhook_1.IncomingWebhook(slackWebhook);
     yield slack.send({
         channel: channel,
-        icon_emoji: icon_emoji,
+        icon_emoji: iconEmoji,
         attachments: [
             {
                 text: [
